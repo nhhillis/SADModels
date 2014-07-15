3351a1d9c663dcb0f4dd826ac0d5c6f72d40c9d1
nhhillis <nhhillis@gmail.com>
2014-07-15 15:26:22 -0500

    Initial work of lognormal

--- Models/LogNormal.py
+++ Models/LogNormal.py
@@ -4,22 +4,31 @@ import numpy as np
 from random import randrange, choice
 import matplotlib.pyplot as plt
 from matplotlib import patches, path
-
+import scipy.stats
 '''This script codes the LogNormal Models'''
 
-# To code the LogNormal...Get distribution, divide into octaves, plot log
-# Still working on this...
-
+# To code the LogNormal...each division is 75:25 
+# Still working on this...numbers are not properly summing to N yet
+# Getting proper number of divisions but whole thing is yet to come together
 
 def SimLogNorm(N, S, sample_size):
-    RAC = []
-    RACs = []
     
-    while len(RACs) < sample_size:
+    for i in range(sample_size):
         
+        sample = []
+        RAC = [N*.75,N*.25]
         
-
-N = 20
-S = 5
-sample_size = 5
+        while len(RAC) < S:
+            x = choice(RAC)
+            new1 = x * .75
+            new2 = x * .25
+            RAC.append(x)
+            RAC.append(new1)
+            RAC.append(new2)
+            print RAC
+        sample.append(RAC)
+        
 sample = SimLogNorm(20, 5, 10)
+print sample
+
+
