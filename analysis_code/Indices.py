c26cd2a5f3a13872cdc1c3e5640999ef0bdf5148
nhhillis <nhhillis@gmail.com>
2014-07-17 16:59:55 -0500

    Created indices file, added notes, generated heat map

---
+++ analysis_code/Indices.py
@@ -0,0 +1,35 @@
+from __future__ import division
+#This code calculates diversity and Evenness
+#This code is in the beginning stages 
+#Simpson's index = D = sum(ni[ni-1])/(N[N-1])
+######Steps######
+#Select each species as n, for each n multiply n * n-1
+#sum all n(n-1) then divide by N(N-1)
+#D = Simpson's Index
+#SD = Simpson's index of Diversity
+#Evns = Simpson's measure of Evenness
+
+def SimpsonD(RAC):
+
+    n1 = 0
+    N = sum(RAC)
+    
+    for n in RAC:
+        n1 += n * (n-1)
+
+    D = n1/(N*(N-1))
+    SD = 1 - D
+    
+    return SD
+
+def SimpsonE(RAC):
+    D = SimpsonD(RAC) + 1     
+    D = 1/D 
+    return D/len(RAC) #Evenness (Magurran 2004)
+    
+def BergerP(RAC):
+    return max(RAC)/sum(RAC)
+    
+RAC = [10,1,1]
+
+print BergerP(RAC)  
