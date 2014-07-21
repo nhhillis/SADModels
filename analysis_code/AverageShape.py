3d4814a23526accefe60949a2ac44a08923710e8
Nathan Hillis <nhhillis@gmail.com>
2014-07-21 00:20:53 -0500

    Worked out some bugs, I think

--- analysis_code/AverageShape.py
+++ analysis_code/AverageShape.py
@@ -2,21 +2,14 @@ from __future__ import division
 import numpy as np
 import sys  
 sys.path.append('/users/Nathan_Hillis/SADModels/models') 
-import LogNormal as LN
-import BrokenStick as BS
-
-bs = BS.SimBrokenStick(100, 5, 5) #Just used for data to use
-logn = LN.SimLogNorm(100, 5, 5)
-
-BSSample = np.array(bs) #converted to numpy arrays 
-LNSample = np.array(logn)
 
 #Working on code to find the average RAC 
 
 def avg(sample):
-    mean = sum(sample)/float(len(sample))
+    x = np.array(sample)
+    mean = sum(x)/float(len(x))
     return mean
-    
+
 
 
 
