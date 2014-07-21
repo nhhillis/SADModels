656ef136a47c32b518571f4f233efb65d5616131
Nathan Hillis <nhhillis@gmail.com>
2014-07-21 00:35:33 -0500

    This seems to be working to find the average

--- analysis_code/AverageShape.py
+++ analysis_code/AverageShape.py
@@ -1,17 +1,9 @@
 from __future__ import division                                        
 import numpy as np
-import sys  
-sys.path.append('/users/Nathan_Hillis/SADModels/models') 
+#Working on code to find the average RAC shape
 
-#Working on code to find the average RAC 
 
 def avg(sample):
     x = np.array(sample)
     mean = sum(x)/float(len(x))
     return mean
-
-
-
-
-
-
