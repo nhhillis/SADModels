32824dca8f30bd39aa71c79b0c7793d4c7436062
Nathan Hillis <nhhillis@gmail.com>
2014-07-26 14:11:32 -0500

    more attempts

--- analysis_code/AverageShape.py
+++ analysis_code/AverageShape.py
@@ -8,16 +8,16 @@ def AvgAbun(sample): #Total abundance / Species
     return np.mean(x)  # this = average abundance
    
 #To find the average shape of the SAD:
-# - compare each SAD to each other SAD and find which one is 
-#   most similar to the others
-# - compare S in list to the average of the other Ss in other lists
-# - OR do you just find which SAD has the most ranks in common with the
-#     others?
-SADs = [[5,3,1,1],[6,2,1,1],[4,3,2,1], [7,1,1,1]]
+# - Find average difference of ranks between 2 RACs (for all combinations 
+# - of RACs).  Find RAC with smallest avg difference.  
+RACs = [[5,3,1,1],[6,2,1,1],[4,3,2,1], [7,1,1,1]]
 
 def AvgShape(sample):
-    return [rank for rank in sample[0] if rank in sample[1]]
     
-            
-print AvgShape(SADs)     
+    for RAC in sample:
+    
+        for rank in RAC: 
+               return cmp(RAC[-1], RAC[-2])
+               
+print AvgShape(RACs)     
     
