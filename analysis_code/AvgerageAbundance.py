55e9930ae43dbd456f203a835a539929cbb0b463
Nathan Hillis <nhhillis@gmail.com>
2014-07-26 16:59:55 -0500

    Cleaned Repo

--- analysis_code/AvgerageAbundance.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division                                        
-import numpy as np
-#Working on code to find the average RAC shape
-
-
-def AvgAbun(sample): #Total abundance / Species
-    x = np.array(sample)
-    return np.mean(x)  # this = average abundance
-   
