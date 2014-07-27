7911f08d080522754b1669ab60f764ca75f5c405
Nathan Hillis <nhhillis@gmail.com>
2014-07-27 10:04:34 -0500

    Added labels and fixed spacing

--- RandFracFigs.py
+++ RandFracFigs.py
@@ -12,44 +12,59 @@ import matplotlib.pyplot as plt
 
 N = 1000
 S = 50
-sample_size = 200
+sample_size = 120
 
 fig = plt.figure()
 
 ax = fig.add_subplot(3, 3, 1)
 BrkStk = Models.SimBrokenStick(N, S, sample_size)
 fig = HeatMap.RACHeatMap(fig, BrkStk)
-plt.title('Broken Stick', fontsize = 10)
-plt.xlabel(...
-plt.ylabel(...
+plt.title('Broken Stick', fontsize = 13)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
 
 print 'finished broken stick'
 
-#ax = fig.add_subplot(3, 2, 2)  # Debug me
-#DPI = Models.DomPreInt(N, S, sample_size)
-#fig = HeatMap.RACHeatMap(fig, DPI)
-#print 'finished dominance preemption (integers)'
+ax = fig.add_subplot(3, 2, 2)  # Debug me
+DPI = Models.DomPreInt(N, S, sample_size)
+fig = HeatMap.RACHeatMap(fig, DPI)
+plt.title('Dominance Preemption (Integer)', fontsize = 10)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
+print 'finished dominance preemption (integers)' 
 
 ax = fig.add_subplot(3, 3, 4)
 DPF = Models.DomPreFloat(N, S, sample_size)
 fig = HeatMap.RACHeatMap(fig, DPF)
+plt.title('Dominance Preemption (Float)', fontsize = 13)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
 print 'finished dominance preemption (decimals)'
 
 ax = fig.add_subplot(3, 3, 5)
 SLN = Models.SimLogNorm(N, S, sample_size)
 fig = HeatMap.RACHeatMap(fig, SLN)
+plt.title('Log Noramal', fontsize = 13)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
 print 'finished lognormal'
 
 ax = fig.add_subplot(3, 3, 7)
 SP = Models.SimPareto(N, S, sample_size, integer=False)
 fig = HeatMap.RACHeatMap(fig, SP)
+plt.title('Pareto', fontsize = 13)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
 print 'finished Pareto'
 
 ax = fig.add_subplot(3, 3, 8)
 SRF = Models.Sample_SimpleRandomFraction(N, S, sample_size)
 fig = HeatMap.RACHeatMap(fig, SRF)
+plt.title('Simple Random Fraction', fontsize = 13)
+plt.xlabel('Rank')
+plt.ylabel('Abundance')
 print 'finished simple random fraction'
 
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
-plt.savefig(...
+plt.subplots_adjust(wspace=0.8, hspace=0.8)
+plt.savefig('Figure_1')
 plt.show()
