1c7d20fd73711fb760a6e48acd4c061037c9c552
Nathan Hillis <nhhillis@gmail.com>
2015-03-12 15:21:21 -0500

    working on test.py

--- Models/test.py
+++ Models/test.py
@@ -1,5 +1,7 @@
 '''Test file'''
 
-#This file will test the model code
+#This file will test the model.py code
 
+'''insert test sads to make sure code is working.
+Ex. Negative numbers, include 0's ect. '''
 
