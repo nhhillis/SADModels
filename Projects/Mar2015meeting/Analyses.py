ef91e62492c29bb1e2481766576fa7dd7138c2b7
Nathan Hillis <nhhillis@gmail.com>
2015-03-17 10:43:33 -0500

    Working on analysis

--- Projects/Mar2015meeting/Analyses.py
+++ Projects/Mar2015meeting/Analyses.py
@@ -5,33 +5,12 @@ import matplotlib.pyplot as plt
 import math
 import sys
 import os
+from scipy import stats
 
-mydir = os.path.expanduser("~/GitHub/SADModels/") # A general file path; this means that:
-# 1.) You will need to create a GitHub directory in your home directory
-# 2.) Move SADModels into the GitHub directory
-
-
-sys.path.append(mydir + "Analysis")
-
-from AverageShape import AvgShape
-
-sys.path.append(mydir + 'Models/')
-import Models
+mydir = os.path.expanduser("~/GitHub/SADModels/") 
 
 sys.path.append(mydir + '/Projects/Mar2015meeting/functions.py')
 
-'''This code will do the following
-1. Obtain observed SADs for ...
-2. For each observed SAD, obtain predicted SADs for each model.
-3. Write data to an "obs_pred.txt" file; with a different file for each model
-    Each obs_pred.txt file will have these rows: Date, Site, Species, ObsAb, PredAB
-'''
 
-'''
-- As of now, need to call model functions instead of copying them (fixed this)
-- Add test functions, e.g. check that the same SAD isn't drawn twice; sample w/out replacement
-- Set up data so that it will work properly --yes, but needs clarification
-- read observed sad data from .txt or .csv files.
-'''
 
 
