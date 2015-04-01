bdd29dd236578a27c0d892ee4e9d926764be0ba9
klocey <ken@weecology.org>
2015-04-01 10:47:44 -0400

    renamed functions.py as MakeObsPred.py, heavily editing MakeObsPred.py, added import_obs_pred_data function to Analyses.py, Sample_SimpleRandomFraction() is now SimpleRandomFraction() -KL

--- Projects/Mar2015meeting/Analyses.py
+++ Projects/Mar2015meeting/Analyses.py
@@ -7,10 +7,18 @@ import sys
 import os
 from scipy import stats
 
-mydir = os.path.expanduser("~/GitHub/SADModels/") 
+mydir = os.path.expanduser("~/GitHub/SADModels/")
 
 sys.path.append(mydir + '/Projects/Mar2015meeting/functions.py')
 
 
 
 
+def import_obs_pred_data(input_filename):
+    # TAKEN FROM THE mete_sads.py script used for White et al. (2012)
+
+    data = np.genfromtxt(input_filename, dtype = "S15, S15, S15, f8, f8",
+    names = ['date','site','species','obs','pred'], delimiter = " ")
+
+    # ensure the delimiter is correct
+    return data
