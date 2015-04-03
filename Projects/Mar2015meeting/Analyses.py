0862fcb457d0d46d304ee5be24bdc1cbccb91b25
Nathan Hillis <nhhillis@gmail.com>
2015-04-03 13:09:20 -0500

    Working on Analysis.py added kernal density functions, need to make the functions work for my data

--- Projects/Mar2015meeting/Analyses.py
+++ Projects/Mar2015meeting/Analyses.py
@@ -48,8 +48,21 @@ def import_obs_pred_data(input_filename):
     # ensure the delimiter is correct
     return data
     
-
     
+def get_kdens(_list):
+    """ Finds the kernel density function across a sample of SADs """
+        # TAKEN FROM THE feasible_functions.py script used for White et al. (2013)
+
+    density = gaussian_kde(_list)
+    n = len(_list)
+    #xs = np.linspace(min(_list),max(_list),n)
+    xs = np.linspace(0.0,1.0,n)
+    density.covariance_factor = lambda : 0.5
+    density._compute_covariance()
+    D = [xs,density(xs)]
+    return D
+        
+        
 def fig1(SADModels):
     """ This function generates a 2x2 figure of Obs vs. Pred heat maps.
     Each of the four subplots reveals the results for a single model.
@@ -67,8 +80,30 @@ def fig2(SADModels):
     """ This function generates a single figure of kernel density curves.
     Each curve represents the pdf for p-values resulting from a 2-tailed
     Kolmogorov-Smirnov test. """
-
     
+    def kdens_full_feasibles(N,S):
+        # TAKEN FROM THE feasible_functions.py script used for White et al. (2013)
+        '''Working on this.  Just have copied it over at this point'''
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        parts = get_all_partitions(N,S)
+        Evars = Evars_sample(parts) # This could be a sample based on another metric (above)
+        D = get_kdens(Evars)
+        plt.xlim(0.0, 1.0)
+        plt.plot(D[0],D[1],color='black',lw=5)
+        parts = get_all_partitions(N,S+10)
+        Evars = Evars_sample(parts) # This could be a sample based on another metric (above)
+        D = get_kdens(Evars)
+        plt.xlim(0.0, 1.0)
+        plt.plot(D[0],D[1],color='gray',lw=5)
+        plt.axvline(x=0.673,ymin=0,ymax=10,color='black',ls='--',lw=3) # plot a vertical line at the mode
+        plt.setp(ax, xticks=[0.2,0.6,1.0],yticks=[0,2,4,6])
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label.set_fontsize(15)
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label.set_fontsize(15)
+        plt.savefig('Figure3.png', dpi=400, pad_inches=0) 
+        
 
 
 
